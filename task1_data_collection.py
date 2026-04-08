2386dcaa7f49dcc5ac1e37f7a489781ec242d215
VijayKumar.Panga <vijayKumar.panga@entainindia.com>
2026-04-08 19:41:30 +0530

    Task1 data fetching

---
+++ task1_data_collection.py
@@ -0,0 +1,57 @@
+import datetime
+import json
+import time
+import os
+import requests
+import urllib3.exceptions
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+headers = {"User-Agent": "TrendPulse/1.0"}
+# fetching ids from the provided hackers endpoint
+resp = requests.get('https://hacker-news.firebaseio.com/v0/topstories.json', headers=headers, verify=False)
+top_stories_ids = resp.json()
+
+# Storing categories and keywords for filtering the stories
+categories = {
+'technology' : ["AI", "software", "tech", "code", "computer", "data", "cloud", "API", "GPU", "LLM"],
+'worldnews' : ["war", "government", "country", "president", "election", "climate", "attack", "global"],
+'sports' : ["NFL", "NBA", "FIFA", "sport", "game", "team", "player", "league", "championship"],
+'science' : ["research", "study", "space", "physics", "biology", "discovery", "NASA", "genome"],
+'entertainment' : ["movie", "film", "music", "Netflix", "game", "book", "show", "award", "streaming"]
+}
+records = []
+
+for id in top_stories_ids:
+    try:
+        resp_ = requests.get(url=f'https://hacker-news.firebaseio.com/v0/item/{id}.json',headers=headers,verify=False)
+    except requests.exceptions.RequestException as e:
+        print(f"Request failed for story {id}: {e}")
+        continue
+    if resp_.status_code == 200:
+        story_details = resp_.json()
+    else:
+        print(f"Failed to fetch story {id}: {resp_.status_code} \n  {resp_.text}")
+        continue
+
+    for category, keywords in categories.items():
+        title = story_details['title'].lower()
+        if any(keyword.lower() in title for keyword in keywords):
+            records.append({'post_id': id,
+                            'title': story_details.get('title', ''),
+                            'category': category,
+                            'score': story_details.get('score', ''),
+                            'num_comments': story_details.get('descendants', ''),
+                            'author': story_details.get('by', ''),
+                            'url': story_details.get('url', ''),
+                            'collected_at': datetime.datetime.now().isoformat()})
+    time.sleep(2)
+
+# creating a data directory if it's not created
+os.makedirs('data', exist_ok = True)
+# setting file path to store the stories data in JSON file
+file_path = f'data/task1_data.json'
+# Opened file in write mode and updated the file with fetched story details
+with open(file_path, 'w') as f:
+    json.dump(records, f, indent = 2)
